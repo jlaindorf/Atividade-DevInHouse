d173b035ef77c325f38a5e9a34d8bfcd1f073aea
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-08-10 13:28:13 -0300

    adiciona About.vue

--- atividade-m1s10/src/App.vue
+++ atividade-m1s10/src/App.vue
@@ -1,47 +1,25 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <form action="" class="form-menu">
+    <div>
+     <button type="submit">Menu</button>
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <div>
+     <button type="submit">Talentos</button>
+    </div>
+    <div>
+     <button type="submit">Sobre</button>
+    </div>
+
+
+  </form>
 </template>
 
+<script>
+
+
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
